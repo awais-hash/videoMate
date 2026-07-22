b0a1f6d14a92394892e993feefed00894acf8513
awais-hash <awaisjust000@gmail.com>
2026-07-22 16:51:56 +0500

    feat: reorganize app.js for improved route management and error handling

--- src/app.js
+++ src/app.js
@@ -1,19 +1,44 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import userRoute from "./routes/user.route.js";
+import videoRouter from "./routes/video.route.js";
+import commentRouter from "./routes/comment.route.js";
+import likeRouter from "./routes/like.route.js";
+import playlistRouter from "./routes/playlist.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import tweetRouter from "./routes/tweet.route.js";
+import healthRouter from "./routes/health.route.js";
+
 
 const app = express()
 
-app.use(express.json({limit: '20kb'}))
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
 }))
+app.use(express.json({limit: '20kb'}))
 app.use(express.urlencoded({extended: true, limit: '20kb'}))
 app.use(cookieParser())
 
-import userRoute from "./routes/user.route.js";
-app.use("/api/v1/users", userRoute);
+app.use("/api/v1/health", healthRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweets", tweetRouter);
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    statusCode: 404,
+  })})
+
+  
+
 
 export {app};
 
