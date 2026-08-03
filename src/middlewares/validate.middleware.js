ea7f6fb6128ead21422f2a17b4bf3de0ee982a4a
awais-hash <awaisjust000@gmail.com>
2026-08-03 18:26:11 +0500

    feat: add validation middleware for request body schema validation

---
+++ src/middlewares/validate.middleware.js
@@ -0,0 +1,19 @@
+import ApiEror from '../utils/apiError.js';
+
+const validate = (schema, source = "body") => {
+  return (req, res, next) => {
+    const result = schema.safeParse(req[source]);
+    if (!result.success) {
+        const errors = result.error.errors.map((issue)=>({
+            field: issue.path.join("."),
+            message: issue.message
+
+        }));
+    return next(new ApiEror(400, "Validation Error", errors));
+
+    }
+
+    req[source] = result.data;
+    next();
+  };
+};
